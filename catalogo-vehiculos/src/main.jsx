662e962a5fe589305f10990f6bff29b31b772b9d
AndresDeJesus <adjzp25@gmail.com>
2025-11-30 11:17:10 -0400

    Creacion-de-vehicleData

--- catalogo-vehiculos/src/main.jsx
+++ catalogo-vehiculos/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import '@mantine/core/styles.css';
+import 'mantine-carousel/styles.css'; 
+import { MantineProvider } from '@mantine/core';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. MantineProvider envuelve toda la App */}
+    <MantineProvider defaultColorScheme="light">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
+);
